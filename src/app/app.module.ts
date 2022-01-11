944f171340085d81ff458d7cd3cccee7bee52006
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-01-11 11:41:49 -0300

    ✨ feat: add toggle button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AnimesComponent } from './animes/animes.component';
 import { CardAnimeComponent } from './card-anime/card-anime.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimesComponent,
-    CardAnimeComponent
+    CardAnimeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mock-animes.ts
+++ src/app/mock-animes.ts
@@ -1,34 +1,126 @@
 import { Anime } from './anime.model';
 
 export const ANIMES: Anime[] = [
-    {day: "Domingo", name: "Shingeki no Kyojin: The Final Season Part 2", img: "https://cdn.myanimelist.net/images/anime/1988/119437.jpg"},
-    {day: "Domingo", name: "Kimetsu no Yaiba: Yuukaku-hen", img: "https://cdn.myanimelist.net/images/anime/1908/120036.jpg"},
-    {day: "Domingo", name: "Futsal Boys!!!!!", img: "https://cdn.myanimelist.net/images/anime/1475/120394.jpg"},
-    {day: "Domingo", name: "Sasaki to Miyano", img: "https://cdn.myanimelist.net/images/anime/1182/119308.jpg"},
-    {day: "Sábado", name: "Shikkakumon no Saikyou Kenja", img: "https://cdn.myanimelist.net/images/anime/1132/120388.jpg"},
-    {day: "Sábado", name: "Sono Bisque Doll wa Koi wo Suru", img: "https://cdn.myanimelist.net/images/anime/1179/119897.jpg"},
-    {day: "Sábado", name: "Akebi-chan no Sailor-fuku", img: "https://cdn.myanimelist.net/images/anime/1312/120268.jpg"},
-    {day: "Sábado", name: "Genjitsu Shugi Yuusha no Oukoku Saikenki 2nd Season", img: "https://cdn.myanimelist.net/images/anime/1088/120068.jpg"},
-    {day: "Sábado", name: "Kaijin Kaihatsu-bu no Kuroitsu-san", img: "https://cdn.myanimelist.net/images/anime/1367/120393.jpg"},
-    {day: "Segunda", name: "Tribe Nine", img: "https://cdn.myanimelist.net/images/anime/1050/118367.jpg"},
-    {day: "Segunda", name: "Sabikui Bisco", img: "https://cdn.myanimelist.net/images/anime/1446/118840.jpg"},
-    {day: "Segunda", name: "Princess Connect! Re:Dive Season 2", img: "https://cdn.myanimelist.net/images/anime/1906/117145.jpg"},
-    {day: "Sexta", name: "Karakai Jouzu no Takagi-san 3", img: "https://cdn.myanimelist.net/images/anime/1861/120361.jpg"},
-    {day: "Quarta", name: "Tensai Ouji no Akaji Kokka Saisei Jutsu", img: "https://cdn.myanimelist.net/images/anime/1263/119511.jpg"},
-    {day: "Quinta", name: "Koroshi Ai", img: "https://cdn.myanimelist.net/images/anime/1485/119329.jpg"},
-    {day: "Quinta", name: "Orient", img: "https://cdn.myanimelist.net/images/anime/1576/119361.jpg"},
-    {day: "Sexta", name: "Kenja no Deshi wo Nanoru Kenja", img: "https://cdn.myanimelist.net/images/anime/1583/119223.jpg"},
-    {day: "Domingo", name: "Baraou no Souretsu", img: "https://cdn.myanimelist.net/images/anime/1046/118527.jpg"},
-    {day: "Sábado", name: "Girls' Frontline", img: "https://cdn.myanimelist.net/images/anime/1783/118956.jpg"},
-    {day: "Quinta", name: "Tokyo 24-ku", img: "https://cdn.myanimelist.net/images/anime/1442/120112.jpg"},
-    {day: "Sexta", name: "Slow Loop", img: "https://cdn.myanimelist.net/images/anime/1360/119608.jpg"},
-    {day: "Quarta", name: "Hakozume: Kouban Joshi no Gyakushuu", img: "https://cdn.myanimelist.net/images/anime/1302/119917.jpg"},
-    {day: "Quarta", name: "Fantasy Bishoujo Juniku Ojisan to", img: "https://cdn.myanimelist.net/images/anime/1430/120065.jpg"},
-    {day: "Quarta", name: "Platinum End", img: "https://cdn.myanimelist.net/images/anime/1992/116576.jpg"},
-    {day: "Sábado", name: "Cue!", img: "https://cdn.myanimelist.net/images/anime/1039/120173.jpg"},
-    {day: "Terça", name: "Gensou Sangokushi: Tengen Reishinki", img: "https://cdn.myanimelist.net/images/anime/1821/116864.jpg"},
-    {day: "Quinta", name: "Saiyuuki Reload: Zeroin", img: "https://cdn.myanimelist.net/images/anime/1451/119862.jpg"},
-    {day: "Quarta", name: "Irodorimidori", img: "https://cdn.myanimelist.net/images/anime/1635/120395.jpg"},
-    {day: "Sexta", name: "Shuumatsu no Harem", img: "https://cdn.myanimelist.net/images/anime/1491/117296.jpg"},
-    {day: "Sexta", name: "Ousama Ranking", img: "https://cdn.myanimelist.net/images/anime/1347/117616.jpg"},
+    {
+        day: "Domingo", 
+        name: "Shingeki no Kyojin: The Final Season Part 2", 
+        img: "https://cdn.myanimelist.net/images/anime/1988/119437.jpg"
+    },
+    {
+        day: "Domingo", 
+        name: "Kimetsu no Yaiba: Yuukaku-hen", 
+        img: "https://cdn.myanimelist.net/images/anime/1908/120036.jpg"
+    },
+    {
+        day: "Domingo", 
+        name: "Futsal Boys!!!!!", 
+        img: "https://cdn.myanimelist.net/images/anime/1475/120394.jpg"},
+    {
+        day: "Domingo", 
+        name: "Sasaki to Miyano", 
+        img: "https://cdn.myanimelist.net/images/anime/1182/119308.jpg"},
+    {
+        day: "Sábado", 
+        name: "Shikkakumon no Saikyou Kenja", 
+        img: "https://cdn.myanimelist.net/images/anime/1132/120388.jpg"},
+    {
+        day: "Sábado",
+        name: "Sono Bisque Doll wa Koi wo Suru", 
+        img: "https://cdn.myanimelist.net/images/anime/1179/119897.jpg"},
+    {
+        day: "Sábado", 
+        name: "Akebi-chan no Sailor-fuku", 
+        img: "https://cdn.myanimelist.net/images/anime/1312/120268.jpg"},
+    {
+        day: "Sábado", 
+        name: "Genjitsu Shugi Yuusha no Oukoku Saikenki 2nd Season", 
+        img: "https://cdn.myanimelist.net/images/anime/1088/120068.jpg"},
+    {
+        day: "Sábado", 
+        name: "Kaijin Kaihatsu-bu no Kuroitsu-san", 
+        img: "https://cdn.myanimelist.net/images/anime/1367/120393.jpg"},
+    {
+        day: "Segunda", 
+        name: "Tribe Nine", 
+        img: "https://cdn.myanimelist.net/images/anime/1050/118367.jpg"},
+    {
+        day: "Segunda", 
+        name: "Sabikui Bisco", 
+        img: "https://cdn.myanimelist.net/images/anime/1446/118840.jpg"},
+    {
+        day: "Segunda", 
+        name: "Princess Connect! Re:Dive Season 2", 
+        img: "https://cdn.myanimelist.net/images/anime/1906/117145.jpg"},
+    {
+        day: "Sexta", 
+        name: "Karakai Jouzu no Takagi-san 3", 
+        img: "https://cdn.myanimelist.net/images/anime/1861/120361.jpg"},
+    {
+        day: "Quarta", 
+        name: "Tensai Ouji no Akaji Kokka Saisei Jutsu", 
+        img: "https://cdn.myanimelist.net/images/anime/1263/119511.jpg"},
+    {
+        day: "Quinta", 
+        name: "Koroshi Ai", 
+        img: "https://cdn.myanimelist.net/images/anime/1485/119329.jpg"},
+    {
+        day: "Quinta", 
+        name: "Orient", 
+        img: "https://cdn.myanimelist.net/images/anime/1576/119361.jpg"},
+    {
+        day: "Sexta", 
+        name: "Kenja no Deshi wo Nanoru Kenja", 
+        img: "https://cdn.myanimelist.net/images/anime/1583/119223.jpg"},
+    {
+        day: "Domingo", 
+        name: "Baraou no Souretsu", 
+        img: "https://cdn.myanimelist.net/images/anime/1046/118527.jpg"},
+    {
+        day: "Sábado", 
+        name: "Girls' Frontline", 
+        img: "https://cdn.myanimelist.net/images/anime/1783/118956.jpg"},
+    {
+        day: "Quinta", 
+        name: "Tokyo 24-ku", 
+        img: "https://cdn.myanimelist.net/images/anime/1442/120112.jpg"},
+    {
+        day: "Sexta", 
+        name: "Slow Loop", 
+        img: "https://cdn.myanimelist.net/images/anime/1360/119608.jpg"},
+    {
+        day: "Quarta", 
+        name: "Hakozume: Kouban Joshi no Gyakushuu", 
+        img: "https://cdn.myanimelist.net/images/anime/1302/119917.jpg"},
+    {
+        day: "Quarta", 
+        name: "Fantasy Bishoujo Juniku Ojisan to", 
+        img: "https://cdn.myanimelist.net/images/anime/1430/120065.jpg"},
+    {
+        day: "Quarta", 
+        name: "Platinum End", 
+        img: "https://cdn.myanimelist.net/images/anime/1992/116576.jpg"},
+    {
+        day: "Sábado", 
+        name: "Cue!", 
+        img: "https://cdn.myanimelist.net/images/anime/1039/120173.jpg"},
+    {
+        day: "Terça", 
+        name: "Gensou Sangokushi: Tengen Reishinki", 
+        img: "https://cdn.myanimelist.net/images/anime/1821/116864.jpg"},
+    {
+        day: "Quinta", 
+        name: "Saiyuuki Reload: Zeroin", 
+        img: "https://cdn.myanimelist.net/images/anime/1451/119862.jpg"},
+    {
+        day: "Quarta", 
+        name: "Irodorimidori", 
+        img: "https://cdn.myanimelist.net/images/anime/1635/120395.jpg"},
+    {
+        day: "Sexta", 
+        name: "Shuumatsu no Harem", 
+        img: "https://cdn.myanimelist.net/images/anime/1491/117296.jpg"},
+    {
+        day: "Sexta", 
+        name: "Ousama Ranking", 
+        img: "https://cdn.myanimelist.net/images/anime/1347/117616.jpg"},
 ];
