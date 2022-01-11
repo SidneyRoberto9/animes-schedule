dbb1b1eef52d2a61fb227517fcc347322a2a2986
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-01-11 19:08:12 -0300

    ✨ feat: cards details urls

--- src/app/components/animes/anime.model.ts
+++ src/app/components/animes/anime.model.ts
@@ -2,4 +2,12 @@ export interface Anime{
     day: string;
     name: string;
     img: string;
+    studio: string;
+    genre: string;
+    sites:{
+        site1: string;
+        site2: string;
+        site3: string;
+        site4: string;
+    }
 }
--- src/app/mocks/mock-animes.ts
+++ src/app/mocks/mock-animes.ts
@@ -4,123 +4,378 @@ export const ANIMES: Anime[] = [
     {
         day: "Domingo", 
         name: "Shingeki no Kyojin: The Final Season Part 2", 
-        img: "https://cdn.myanimelist.net/images/anime/1988/119437.jpg"
+        img: "https://cdn.myanimelist.net/images/anime/1988/119437.jpg",
+        studio: "MAPPA",
+        genre: "Action, Drama, Fantasy, Mystery, Military, Super Power",
+        sites:{
+            site1: "https://animeshouse.net/anime/shingeki-no-kyojin/",
+            site2: "https://animesonline.cc/anime/shingeki-no-kyojin/",
+            site3: "https://animesonline.org/animes/shingeki-no-kyojin/",
+            site4: "https://betteranime.net/anime/legendado/shingeki-no-kyojin-the-final-season-part-2"
+        }
     },
     {
         day: "Domingo", 
         name: "Kimetsu no Yaiba: Yuukaku-hen", 
-        img: "https://cdn.myanimelist.net/images/anime/1908/120036.jpg"
+        img: "https://cdn.myanimelist.net/images/anime/1908/120036.jpg",
+        studio: "ufotable",
+        genre: "Action, Supernatural, Demons, Historical",
+        sites:{
+            site1: "https://animeshouse.net/anime/kimetsu-no-yaiba/",
+            site2: "https://animesonline.cc/anime/kimetsu-no-yaiba/",
+            site3: "https://animesonline.org/animes/kimetsu-no-yaiba/",
+            site4: "https://betteranime.net/anime/legendado/kimetsu-no-yaiba-yuukaku-hen"
+        }
     },
     {
         day: "Domingo", 
         name: "Futsal Boys!!!!!", 
-        img: "https://cdn.myanimelist.net/images/anime/1475/120394.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1475/120394.jpg",
+        studio: "Diomedéa",
+        genre: "Sports",
+        sites:{
+            site1: "https://animeshouse.net/anime/futsal-boys/",
+            site2: "https://animefire.net/animes/futsal-boys-todos-os-episodios",
+            site3: "https://animesonline.org/animes/futsal-boys/",
+            site4: "https://betteranime.net/anime/legendado/futsal-boys"
+        }
+    },
     {
         day: "Domingo", 
         name: "Sasaki to Miyano", 
-        img: "https://cdn.myanimelist.net/images/anime/1182/119308.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1182/119308.jpg",
+        studio: "Studio Deen",
+        genre: "Boys Love, Slice of Life, School",
+        sites:{
+            site1: "https://animeshouse.net/anime/sasaki-to-miyano/",
+            site2: "https://animefire.net/animes/sasaki-to-miyano-todos-os-episodios",
+            site3: "https://animesonline.org/animes/sasaki-to-miyano/",
+            site4: "https://betteranime.net/anime/legendado/sasaki-to-miyano"
+        }
+    },
     {
         day: "Sábado", 
         name: "Shikkakumon no Saikyou Kenja", 
-        img: "https://cdn.myanimelist.net/images/anime/1132/120388.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1132/120388.jpg",
+        studio: "J.C.Staff",
+        genre: "Action, Adventure, Fantasy",
+        sites:{
+            site1: "https://animeshouse.net/anime/shikkakumon-no-saikyou-kenja/",
+            site2: "https://animesonline.cc/anime/shikkakumon-no-saikyou-kenja/",
+            site3: "https://animesonline.org/animes/shikkakumon-no-saikyou-kenja/",
+            site4: "https://betteranime.net/anime/legendado/shikkakumon-no-saikyou-kenja"
+        }
+    },
     {
         day: "Sábado",
         name: "Sono Bisque Doll wa Koi wo Suru", 
-        img: "https://cdn.myanimelist.net/images/anime/1179/119897.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1179/119897.jpg",
+        studio: "CloverWorks",
+        genre: "Romance, Slice of Life, School, Seinen",
+        sites:{
+            site1: "https://animeshouse.net/anime/sono-bisque-doll-wa-koi-wo-suru/",
+            site2: "https://animesonline.cc/anime/sono-bisque-doll-wa-koi-wo-suru/",
+            site3: "https://animesonline.org/animes/sono-bisque-doll-wa-koi-wo-suru/",
+            site4: "https://betteranime.net/anime/legendado/sono-bisque-doll-wa-koi-wo-suru"
+        }
+    },
     {
         day: "Sábado", 
         name: "Akebi-chan no Sailor-fuku", 
-        img: "https://cdn.myanimelist.net/images/anime/1312/120268.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1312/120268.jpg",
+        studio: "CloverWorks",
+        genre: "Slice of Life, School",
+        sites:{
+            site1: "https://animeshouse.net/anime/akebi-chan-no-sailor-fuku/",
+            site2: "https://animesonline.cc/anime/akebi-chan-no-sailor-fuku/",
+            site3: "https://animesonline.org/animes/akebi-chan-no-sailor-fuku/",
+            site4: "https://betteranime.net/anime/legendado/akebi-chan-no-sailor-fuku"
+        }
+    },
     {
         day: "Sábado", 
         name: "Genjitsu Shugi Yuusha no Oukoku Saikenki 2nd Season", 
-        img: "https://cdn.myanimelist.net/images/anime/1088/120068.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1088/120068.jpg",
+        studio: "J.C.Staff",
+        genre: "Action, Fantasy, Romance",
+        sites:{
+            site1: "https://animeshouse.net/anime/genjitsu-shugi-yuusha-no-oukoku-saikenki/",
+            site2: "https://animesonline.cc/anime/genjitsu-shugi-yuusha-no-oukoku-saikenki/",
+            site3: "https://animesonline.org/animes/genjitsu-shugi-yuusha-no-oukoku-saikenki/",
+            site4: "https://betteranime.net/anime/legendado/genjitsu-shugi-yuusha-no-oukoku-saikenki-2nd-season"
+        }
+    },
     {
         day: "Sábado", 
         name: "Kaijin Kaihatsu-bu no Kuroitsu-san", 
-        img: "https://cdn.myanimelist.net/images/anime/1367/120393.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1367/120393.jpg",
+        studio: "Quad",
+        genre: "Comedy, Fantasy",
+        sites:{
+            site1: "https://animeshouse.net/anime/kaijin-kaihatsu-bu-no-kuroitsu-san/",
+            site2: "https://animesonline.cc/anime/kaijin-kaihatsu-bu-no-kuroitsu-san/",
+            site3: "https://animesonline.org/animes/kaijin-kaihatsu-bu-no-kuroitsu-san/",
+            site4: "https://betteranime.net/anime/legendado/kaijin-kaihatsu-bu-no-kuroitsu-san"
+        }
+    },
     {
         day: "Segunda", 
         name: "Tribe Nine", 
-        img: "https://cdn.myanimelist.net/images/anime/1050/118367.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1050/118367.jpg",
+        studio: "LIDENFILMS",
+        genre: "Sports",
+        sites:{
+            site1: "https://animeshouse.net/anime/tribe-nine/",
+            site2: "https://animesonline.cc/anime/tribe-nine/",
+            site3: "https://animesonline.org/animes/tribe-nine/",
+            site4: "https://betteranime.net/anime/legendado/tribe-nine"
+        }
+    },
     {
         day: "Segunda", 
         name: "Sabikui Bisco", 
-        img: "https://cdn.myanimelist.net/images/anime/1446/118840.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1446/118840.jpg",
+        studio: "OZ",
+        genre: "Action, Adventure, Fantasy",
+        sites:{
+            site1: "https://animeshouse.net/anime/sabikui-bisco/",
+            site2: "https://animesonline.cc/anime/sabikui-bisco/",
+            site3: "https://animesonline.org/animes/sabikui-bisco/",
+            site4: "https://betteranime.net/anime/legendado/sabikui-bisco"
+        }
+    },
     {
         day: "Segunda", 
         name: "Princess Connect! Re:Dive Season 2", 
-        img: "https://cdn.myanimelist.net/images/anime/1906/117145.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1906/117145.jpg",
+        studio: "CygamesPictures",
+        genre: "Action, Adventure, Comedy, Fantasy",
+        sites:{
+            site1: "https://animeshouse.net/anime/princess-connect-redive/",
+            site2: "https://animesonline.cc/anime/princess-connect-redive/",
+            site3: "https://animesonline.org/animes/princess-connect-redive/",
+            site4: "https://betteranime.net/anime/legendado/princess-connect-re-dive-season-2"
+        }
+    },
     {
         day: "Sexta", 
         name: "Karakai Jouzu no Takagi-san 3", 
-        img: "https://cdn.myanimelist.net/images/anime/1861/120361.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1861/120361.jpg",
+        studio: "CygamesPictures",
+        genre: "Action, Adventure, Comedy, Fantasy",
+        sites:{
+            site1: "https://animeshouse.net/anime/karakai-jouzu-no-takagi-san/",
+            site2: "https://animesonline.cc/anime/karakai-jouzu-no-takagi-san/",
+            site3: "https://animesonline.org/animes/karakai-jouzu-no-takagi-san/",
+            site4: "https://betteranime.net/anime/legendado/karakai-jouzu-no-takagi-san-3"
+        }
+    },
     {
-        day: "Quarta", 
+        day: "Terça", 
         name: "Tensai Ouji no Akaji Kokka Saisei Jutsu", 
-        img: "https://cdn.myanimelist.net/images/anime/1263/119511.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1263/119511.jpg",
+        studio: "Yokohama Animation Lab",
+        genre: "Comedy, Fantasy",
+        sites:{
+            site1: "https://animeshouse.net/anime/tensai-ouji-no-akaji-kokka-saisei-jutsu/",
+            site2: "https://animesonline.cc/anime/tensai-ouji-no-akaji-kokka-saisei-jutsu/",
+            site3: "https://animesonline.org/animes/tensai-ouji-no-akaji-kokka-saisei-jutsu/",
+            site4: "https://betteranime.net/anime/legendado/tensai-ouji-no-akaji-kokka-saisei-jutsu"
+        }
+    },
     {
         day: "Quinta", 
         name: "Koroshi Ai", 
-        img: "https://cdn.myanimelist.net/images/anime/1485/119329.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1485/119329.jpg",
+        studio: "Platinum Vision",
+        genre: "Action, Romance, Josei",
+        sites:{
+            site1: "",
+            site2: "",
+            site3: "https://animesonline.org/animes/koroshi-ai/",
+            site4: "https://betteranime.net/anime/legendado/koroshi-ai"
+        }
+    },
     {
         day: "Quinta", 
         name: "Orient", 
-        img: "https://cdn.myanimelist.net/images/anime/1576/119361.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1576/119361.jpg",
+        studio: "A.C.G.T.",
+        genre: "Action, Fantasy, Shounen",
+        sites:{
+            site1: "https://animeshouse.net/anime/orient/",
+            site2: "https://animesonline.cc/anime/orient/",
+            site3: "https://animesonline.org/animes/orient/",
+            site4: "https://betteranime.net/anime/legendado/orient-2"
+        }
+    },
     {
-        day: "Sexta", 
+        day: "Terça", 
         name: "Kenja no Deshi wo Nanoru Kenja", 
-        img: "https://cdn.myanimelist.net/images/anime/1583/119223.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1583/119223.jpg",
+        studio: "Studio A-CAT",
+        genre: "Adventure, Fantasy",
+        sites:{
+            site1: "",
+            site2: "https://animefire.net/animes/kenja-no-deshi-wo-nanoru-kenja-todos-os-episodios",
+            site3: "",
+            site4: "https://betteranime.net/anime/legendado/kenja-no-deshi-wo-nanoru-kenja"
+        }
+    },
     {
         day: "Domingo", 
         name: "Baraou no Souretsu", 
-        img: "https://cdn.myanimelist.net/images/anime/1046/118527.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1046/118527.jpg",
+        studio: "J.C.Staff",
+        genre: "Action, Drama, Supernatural, Historical, Shoujo",
+        sites:{
+            site1: "https://animeshouse.net/anime/baraou-no-souretsu/",
+            site2: "https://animesonline.cc/anime/baraou-no-souretsu/",
+            site3: "https://animesonline.org/animes/baraou-no-souretsu/",
+            site4: "https://betteranime.net/anime/legendado/baraou-no-souretsu"
+        }
+    },
     {
         day: "Sábado", 
         name: "Girls' Frontline", 
-        img: "https://cdn.myanimelist.net/images/anime/1783/118956.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1783/118956.jpg",
+        studio: "Asahi Production",
+        genre: "Action, Drama, Sci-Fi, Military",
+        sites:{
+            site1: "https://animeshouse.net/anime/girls-frontline/",
+            site2: "https://animesonline.cc/anime/dolls-frontline/",
+            site3: "https://animesonline.org/animes/dolls-frontline/",
+            site4: "https://betteranime.net/anime/legendado/dolls-frontline"
+        }
+    },
     {
         day: "Quinta", 
         name: "Tokyo 24-ku", 
-        img: "https://cdn.myanimelist.net/images/anime/1442/120112.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1442/120112.jpg",
+        studio: "CloverWorks",
+        genre: "Action, Drama",
+        sites:{
+            site1: "https://animeshouse.net/anime/tokyo-24-ku/",
+            site2: "https://animesonline.cc/anime/tokyo-24-ku/",
+            site3: "https://animesonline.org/animes/tokyo-24-ku/",
+            site4: "https://betteranime.net/anime/legendado/tokyo-24-ku-2"
+        }
+    },
     {
         day: "Sexta", 
         name: "Slow Loop", 
-        img: "https://cdn.myanimelist.net/images/anime/1360/119608.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1360/119608.jpg",
+        studio: "Connect",
+        genre: "Slice of Life, Sports",
+        sites:{
+            site1: "https://animeshouse.net/anime/slow-loop/",
+            site2: "https://animesonline.cc/anime/slow-loop/",
+            site3: "https://animesonline.cc/anime/slow-loop/",
+            site4: "https://betteranime.net/anime/legendado/slow-loop"
+        }
+    },
     {
         day: "Quarta", 
         name: "Hakozume: Kouban Joshi no Gyakushuu", 
-        img: "https://cdn.myanimelist.net/images/anime/1302/119917.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1302/119917.jpg",
+        studio: "Madhouse",
+        genre: "Comedy, Slice of Life, Police, Seinen",
+        sites:{
+            site1: "https://animeshouse.net/anime/hakozume-kouban-joshi-no-gyakushuu/",
+            site2: "https://animesonline.cc/anime/hakozume-kouban-joshi-no-gyakushuu/",
+            site3: "https://animesonline.org/animes/hakozume-kouban-joshi-no-gyakushuu/",
+            site4: "https://betteranime.net/anime/legendado/hakozume-kouban-joshi-no-gyakushuu"
+        }
+    },
     {
-        day: "Quarta", 
+        day: "Terça", 
         name: "Fantasy Bishoujo Juniku Ojisan to", 
-        img: "https://cdn.myanimelist.net/images/anime/1430/120065.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1430/120065.jpg",
+        studio: "OLM Team Yoshioka",
+        genre: "Adventure, Comedy, Fantasy",
+        sites:{
+            site1: "https://animeshouse.net/anime/fantasy-bishoujo-juniku-ojisan-to/",
+            site2: "https://animesonline.cc/anime/fantasy-bishoujo-juniku-ojisan-to/",
+            site3: "https://animesonline.org/animes/fantasy-bishoujo-juniku-ojisan-to/",
+            site4: "https://betteranime.net/anime/legendado/fantasy-bishoujo-juniku-ojisan-to"
+        }
+    },
     {
         day: "Quarta", 
         name: "Platinum End", 
-        img: "https://cdn.myanimelist.net/images/anime/1992/116576.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1992/116576.jpg",
+        studio: "Signal.MD",
+        genre: "Drama, Supernatural, Psychological, Shounen",
+        sites:{
+            site1: "https://animeshouse.net/anime/platinum-end/",
+            site2: "https://animesonline.cc/anime/platinum-end/",
+            site3: "https://animesonline.org/animes/platinum-end/",
+            site4: "https://betteranime.net/anime/legendado/platinum-end"
+        }
+    },
     {
         day: "Sábado", 
         name: "Cue!", 
-        img: "https://cdn.myanimelist.net/images/anime/1039/120173.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1039/120173.jpg",
+        studio: "Yumeta Company, Graphinica",
+        genre: "Music",
+        sites:{
+            site1: "https://animeshouse.net/anime/cue/",
+            site2: "https://animefire.net/animes/cue-todos-os-episodios",
+            site3: "https://animesonline.org/animes/cue/",
+            site4: "https://betteranime.net/anime/legendado/cue"
+        }
+    },
     {
-        day: "Terça", 
+        day: "Segunda", 
         name: "Gensou Sangokushi: Tengen Reishinki", 
-        img: "https://cdn.myanimelist.net/images/anime/1821/116864.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1821/116864.jpg",
+        studio: "GEEK TOYS",
+        genre: "Fantasy, Historical",
+        sites:{
+            site1: "https://animeshouse.net/anime/gensou-sangokushi-tengen-reishinki/",
+            site2: "https://animesonline.cc/anime/gensou-sangokushi-tengen-reishinki/",
+            site3: "https://animesonline.org/animes/gensou-sangokushi-tengen-reishinki/",
+            site4: "https://betteranime.net/anime/legendado/gensou-sangokushi-tengen-reishinki"
+        }
+    },
     {
         day: "Quinta", 
         name: "Saiyuuki Reload: Zeroin", 
-        img: "https://cdn.myanimelist.net/images/anime/1451/119862.jpg"},
-    {
-        day: "Quarta", 
-        name: "Irodorimidori", 
-        img: "https://cdn.myanimelist.net/images/anime/1635/120395.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1451/119862.jpg",
+        studio: "LIDENFILMS",
+        genre: "Action, Adventure, Comedy, Drama, Supernatural, Demons, Josei",
+        sites:{
+            site1: "",
+            site2: "https://animefire.net/animes/saiyuuki-reload-zeroin-todos-os-episodios",
+            site3: "",
+            site4: "https://betteranime.net/anime/legendado/saiyuuki-reload-blast"
+        }
+    },
     {
         day: "Sexta", 
         name: "Shuumatsu no Harem", 
-        img: "https://cdn.myanimelist.net/images/anime/1491/117296.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1491/117296.jpg",
+        studio: "Studio Gokumi, AXsiZ",
+        genre: "Sci-Fi, Ecchi, Harem, Shounen",
+        sites:{
+            site1: "https://animeshouse.net/anime/shuumatsu-no-harem/",
+            site2: "https://animesonline.cc/anime/shuumatsu-no-harem/",
+            site3: "https://animesonline.org/animes/shuumatsu-no-harem/",
+            site4: "https://animesonline.org/animes/shuumatsu-no-harem/"
+        }
+    },
     {
         day: "Sexta", 
         name: "Ousama Ranking", 
-        img: "https://cdn.myanimelist.net/images/anime/1347/117616.jpg"},
+        img: "https://cdn.myanimelist.net/images/anime/1347/117616.jpg",
+        studio: "Wit Studio",
+        genre: "Adventure, Fantasy",
+        sites:{
+            site1: "https://animeshouse.net/anime/ousama-ranking/",
+            site2: "https://animesonline.cc/anime/ousama-ranking/",
+            site3: "https://animesonline.org/animes/ousama-ranking/",
+            site4: "https://betteranime.net/anime/legendado/ousama-ranking"
+        }
+    },
 ];
