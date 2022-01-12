761dddbdc20f2147f51d4646b30dcec540ac835a
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-01-12 16:38:47 -0300

    🐛 fix: invalid import ThrowStmt remove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { ThrowStmt } from '@angular/compiler';
 import { Component} from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
