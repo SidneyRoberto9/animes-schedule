3713314062601ea2f5cebc7e65a637d0202edc03
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-01-17 11:07:57 -0300

    🐛 fix: Router to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomePage } from './pages/home/home.page';
 
 const routes: Routes = [
   {path: '', component: HomePage},
+  {path: '**', redirectTo: ''},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
