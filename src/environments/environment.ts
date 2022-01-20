5444a89cd93dafc897d25a21a5b67bcc23768b6f
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-01-20 10:12:00 -0300

    🏗️ build: change npm env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
-
 export const environment = {
-    production: false,
-    APPLICATION_ID: "OUf1RxhywaPnH6Qi5hmQIyEXLzEz3FGtNdoaH7V4",
-    JS_KEY: "myXx7hoFY7GMPEU24KAWq6tDmyW94ij5wVCgvp7u",
+  production: false,
+  APPLICATION_ID: '',
+  JS_KEY: '',
 };
